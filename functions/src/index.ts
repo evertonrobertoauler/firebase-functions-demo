a2239178a3811f0cb97d872af2226c93f442d1de
Everton R. Auler <evertonrobertoauler@gmail.com>
2017-03-20 01:26:53 -0300

    Ajustando funcoes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,7 @@
 import * as functions from 'firebase-functions';
 
-export const onAction = functions.database.ref('actions/{pushId}').onWrite(event => {
-  console.log(event.params, event.data.val());
-});
+export const onAction = functions.database.ref('/actions/{pushId}')
+  .onWrite(async event => {
+    const snapshot = await event.data.ref.root.child('/actions').once('value');
+    console.log(snapshot.val(), event.params, event.data.val());
+  });
